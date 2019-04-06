1f9ad13be77333967c2fabaa2428976c078b0d9a
robert-krivelis <rob_kriv@hotmail.com>
2019-04-06 16:31:04 -0600

    commit!!

--- deploy.py
+++ deploy.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>deployed!</h1>'
+	return render_template("websiteinprogress.html")
