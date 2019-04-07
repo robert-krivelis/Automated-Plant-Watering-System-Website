919fbb5f0cd920b87ee7a7138da22ea8f49ceede
robert-krivelis <rob_kriv@hotmail.com>
2019-04-06 22:25:30 -0600

    Should work actually fr!

--- deploy.py
+++ deploy.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, send_from_directory
-from flask_alchemy import SQLAlchemy
+#from flask_alchemy import SQLAlchemy
 from datetime import datetime
 #datetime.now()
 #yr,month,day,hour,min,second,ms
 app = Flask(__name__)	
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-db = SQLAlchemy(app)
-class plant(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	time = db.Column(db.Integer)
-	moisture = db.Column(db.Integer)
-	temperature = db.Column(db.Real)
-	humidity = db.Column(db.Real)
+# db = SQLAlchemy(app)
+# class plant(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	time = db.Column(db.Integer)
+# 	moisture = db.Column(db.Integer)
+# 	temperature = db.Column(db.Real)
+# 	humidity = db.Column(db.Real)
 
-	def __repr__(self):
-		return f"plant('{self.time}','{self.moisture}','{self.temperature}','{self.humidity}')"
+# 	def __repr__(self):
+# 		return f"plant('{self.time}','{self.moisture}','{self.temperature}','{self.humidity}')"
 
 
 #we're going to need to pass to HTML: datasets(x,y)
