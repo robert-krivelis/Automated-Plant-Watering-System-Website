457e5bad1bb016e975b5d5cfa3ee1448d7fcea94
robert-krivelis <rob_kriv@hotmail.com>
2019-04-07 14:12:22 -0600

    first commit with no connection

--- deploy.py
+++ deploy.py
@@ -1,22 +1,65 @@
 from flask import Flask, render_template, request, send_from_directory
-#from flask_alchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import time
+import requests
 #datetime.now()
 #yr,month,day,hour,min,second,ms
 app = Flask(__name__)	
+#print(today.strftime("%A, %B, %d, %Y"))
+#from deploy import db? not neccesary
+#db.create_all()
+#from deploy import plant #not necesary
+#reading1= Plant(temperature = '21.2')
+#db.session.add(reading1)
+#db.session.commit()
+#Plant.query.all()
+#Plant.query.filter_by(*).all()
+#Plant.query.get(1) - we want to be able to search the database by column
+#Plant.query.oder
+#xvalues, yvalues will be a list
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+start_time = time.perf_counter()
 
-# db = SQLAlchemy(app)
-# class plant(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	time = db.Column(db.Integer)
-# 	moisture = db.Column(db.Integer)
-# 	temperature = db.Column(db.Real)
-# 	humidity = db.Column(db.Real)
+moistURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/moisturePercentage?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
+tempURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/temperature?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
+humidURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/humidity?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
 
-# 	def __repr__(self):
-# 		return f"plant('{self.time}','{self.moisture}','{self.temperature}','{self.humidity}')"
+def getjson(url):
+	r = requests.get(url)
+	out = r.json()['result']
+	return out
+
+if ((time.perf_counter()-start_time)//1%59 == 0):
+	globalmoist = getjson(moistURL)
+if ((time.perf_counter()-start_time)//1%58 == 0):	
+	globalhumid = getjson(humidURL)
+if ((time.perf_counter()-start_time)//1%57 == 0):	
+	globaltemp = getjson(tempURL)
+
+
+if ((time.perf_counter()-start_time)//1%60 == 0):
+	newReading = Plant(moisture = 'globalmoist', temperature = 'globaltemp', humidity = 'globalhumid')
+	db.session.add(newReading)
+	db.session.commit()
+	
+
+xvalues = Plant.query.with_entities(Plant.time).all()
+ytemperature = Plant.query.with_entities(Plant.temperature).all()
+ymoisture = Plant.query.with_entities(Plant.moisture).all()
+yhumidity = Plant.query.with_entities(Plant.humidity).all()
+
+db = SQLAlchemy(app)
+class Plant(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	time = db.Column(db.String, nullable=False, default = datetime.now().strftime("%A, %B %d, %I:%M %p"))
+	moisture = db.Column(db.Integer)
+	temperature = db.Column(db.Float)
+	humidity = db.Column(db.Float)
+
+	def __repr__(self):
+		return f"Plant('{self.time}','{self.moisture}','{self.temperature}','{self.humidity}')"
 
 
 #we're going to need to pass to HTML: datasets(x,y)
