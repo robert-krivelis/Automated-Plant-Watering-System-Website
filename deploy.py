2aaeeb70217782714dcc805708270eb3968cb378
robert-krivelis <rob_kriv@hotmail.com>
2019-04-07 18:08:28 -0600

    2nd update no wifi

--- deploy.py
+++ deploy.py
@@ -18,56 +18,60 @@ app = Flask(__name__)
 #Plant.query.get(1) - we want to be able to search the database by column
 #Plant.query.oder
 #xvalues, yvalues will be a list
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-start_time = time.perf_counter()
+#---------------------------------------
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-moistURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/moisturePercentage?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
-tempURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/temperature?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
-humidURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/humidity?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
+# start_time = time.perf_counter()
 
-def getjson(url):
-	r = requests.get(url)
-	out = r.json()['result']
-	return out
+# moistURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/moisturePercentage?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
+# tempURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/temperature?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
+# humidURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/humidity?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
 
-if ((time.perf_counter()-start_time)//1%59 == 0):
-	globalmoist = getjson(moistURL)
-if ((time.perf_counter()-start_time)//1%58 == 0):	
-	globalhumid = getjson(humidURL)
-if ((time.perf_counter()-start_time)//1%57 == 0):	
-	globaltemp = getjson(tempURL)
+# def getjson(url):
+# 	r = requests.get(url)
+# 	out = r.json()['result']
+# 	return out
+
+# if ((time.perf_counter()-start_time)//1%59 == 0):
+# 	globalmoist = getjson(moistURL)
+# if ((time.perf_counter()-start_time)//1%58 == 0):	
+# 	globalhumid = getjson(humidURL)
+# if ((time.perf_counter()-start_time)//1%57 == 0):	
+# 	globaltemp = getjson(tempURL)
 
 
-if ((time.perf_counter()-start_time)//1%60 == 0):
-	newReading = Plant(moisture = 'globalmoist', temperature = 'globaltemp', humidity = 'globalhumid')
-	db.session.add(newReading)
-	db.session.commit()
+# if ((time.perf_counter()-start_time)//1%60 == 0):
+# 	newReading = Plant(moisture = 'globalmoist', temperature = 'globaltemp', humidity = 'globalhumid')
+# 	db.session.add(newReading)
+# 	db.session.commit()
 	
 
-xvalues = Plant.query.with_entities(Plant.time).all()
-ytemperature = Plant.query.with_entities(Plant.temperature).all()
-ymoisture = Plant.query.with_entities(Plant.moisture).all()
-yhumidity = Plant.query.with_entities(Plant.humidity).all()
+# xvalues = Plant.query.with_entities(Plant.time).all()
+# ytemperature = Plant.query.with_entities(Plant.temperature).all()
+# ymoisture = Plant.query.with_entities(Plant.moisture).all()
+# yhumidity = Plant.query.with_entities(Plant.humidity).all()
 
-db = SQLAlchemy(app)
-class Plant(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	time = db.Column(db.String, nullable=False, default = datetime.now().strftime("%A, %B %d, %I:%M %p"))
-	moisture = db.Column(db.Integer)
-	temperature = db.Column(db.Float)
-	humidity = db.Column(db.Float)
+# db = SQLAlchemy(app)
+# class Plant(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	time = db.Column(db.String, nullable=False, default = datetime.now().strftime("%A, %B %d, %I:%M %p"))
+# 	moisture = db.Column(db.Integer)
+# 	temperature = db.Column(db.Float)
+# 	humidity = db.Column(db.Float)
 
-	def __repr__(self):
-		return f"Plant('{self.time}','{self.moisture}','{self.temperature}','{self.humidity}')"
-
-
+# 	def __repr__(self):
+# 		return f"Plant('{self.time}','{self.moisture}','{self.temperature}','{self.humidity}')"
+#---------------------------------------
+object1 = {'num': [86,114,106,106,107,111,133,221,783,24782], 'rest': 'jemima'}
 #we're going to need to pass to HTML: datasets(x,y)
 #for our colums we're going to need entry(primary), time, moisture, temp, humidity.
 #later we can add ideal moisture, ideal temp, ideal humidity. This is probably easiest if we store all the values
 #in our database and just reference them when we need to.
 @app.route('/')
 def index():
-	return render_template("websiteinprogress.html")
+	return render_template("websiteinprogress.html", object1=object1)# xvalues=xvalues, globalmoist=globalmoist, globaltemp=globaltemp, globalhumid=globalhumid)
+
+	#return render_template("websiteinprogress.html", xvalues=xvalues, globalmoist=globalmoist, globaltemp=globaltemp, globalhumid=globalhumid)
 #if __name__ == '__main__':
 #	app.run(debug=True)
--- templates/history.js
+++ templates/history.js
@@ -1,39 +1,14 @@
 //JSONDATA
-var moistURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/moisturePercentage?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
-var tempURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/temperature?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
-var humidURL = 'https://api.particle.io/v1/devices/350025000b47363339343638/humidity?access_token=1aa09ff2c1a069ca69c16e6822b0602412a8c1c4';
-var globalmoist = {'percent' : null } ;
-var globaltemp = {'percent' : null } ;
-var globalhumid = {'percent' : null } ;
-getjson(moistURL, globalmoist);
-getjson(humidURL, globalhumid);
-getjson(tempURL, globaltemp);
 
-setInterval(getjson(moistURL, globalmoist), 3001);
-setInterval(getjson(humidURL, globalhumid),3002);
-setInterval(getjson(tempURL, globaltemp),3003);
-
-function getjson(url, value){
-  $.getJSON(url, function(data) {
-    console.log(data);
-    (value).percent = data.result;
-    console.log(value)
-    console.log(data.result);
-  });
-}
-var speedCanvas = document.getElementById("speedChart");
+var historyCanvas = document.getElementById("historyChart");
 
 Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.defaultFontSize = 18;
-var cm = globalmoist.percent;
-console.log('Current Value:');
-console.log(cm);
 
-
-var speedData = {
-  labels: [-5, -4, -3, -2, -1, 0],
+var Data = {
+  labels: [ {{globalmoist}} ],
   datasets: [{
-    label: {{currentvalue}}, //change to"Moisture %"
+    label: "Moisture %",
     fill: false,
     data: [cm, cm, cm, cm, cm, cm, cm, cm, cm],
     lineTension: 0.5,
@@ -108,8 +83,8 @@ function adddata(){
 }
 
 
-var lineChart = new Chart(speedCanvas, {
+var lineChart = new Chart(HistoryCanvas, {
   type: 'line',
-  data: speedData,
+  data: Data,
   options: chartOptions
 });
