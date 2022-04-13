5597de94458669d1600236015043339191019482
robert <rob_kriv@hotmail.com>
2022-04-12 20:08:42 -0600

    some error messages working

--- edu/ucalgary/ensf409/Main.java
+++ edu/ucalgary/ensf409/Main.java
@@ -15,14 +15,16 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 import java.awt.event.*;
+import java.net.URL;
 import java.util.*;
 import java.awt.*;
 
 public class Main extends JFrame implements ActionListener, MouseListener {
 
+    public ArrayList<ArrayList<Integer>> allClientTypesFromUser = new ArrayList<ArrayList<Integer>>();
     private JLabel instructions;
 
-    private int numberOfHampersVisible = 1;
+    private int numberOfHampersVisible = 0;
 
     // STORE LABELS HERE
     private ArrayList<JLabel> hamperNumberLabel = new ArrayList<JLabel>();
@@ -47,45 +49,17 @@ public class Main extends JFrame implements ActionListener, MouseListener {
 
     public Main() {
         super("Hamper Order Form "); // extends Jframe - sets the title of the window
-        // initializeGUILabels();
         setupGUI(); // create GUI
         setSize(500, 300); // sets window size
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    // private void initializeGUILabels(){
-    // ArrayList<JLabel>[] = new ArrayList<JLabel>
-    // for (arr:)
-    // }
-
     public void addNewHamper() {
         // create additional hamper
         if (numberOfHampersVisible < 5) {// max limit of 5 for number of hampers for now since it just needs "several"
-            hamperNumberLabel.add(new JLabel("Hamper " + String.valueOf(numberOfHampersVisible + 1) + ":"));
-            Font font = new Font("Default", Font.BOLD, 16);
-            hamperNumberLabel.get(numberOfHampersVisible).setFont(font);
-            this.clientPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // padding to make it look not as packed
 
             // refactor into new function later
-            adultMalesInput.add(new JTextField("0", 15));
-            adultFemalesInput.add(new JTextField("0", 15));
-            childrenOverEightInput.add(new JTextField("0", 15));
-            childrenUnderEightInput.add(new JTextField("0", 15));
-            adultMalesLabel.add(new JLabel("Adult Males:"));
-            adultFemalesLabel.add(new JLabel("Adult Females:"));
-            childrenOverEightLabel.add(new JLabel("Children over 8:"));
-            childrenUnderEightLabel.add(new JLabel("Children under 8:"));
-
-            this.clientPanel.add(hamperNumberLabel.get(numberOfHampersVisible));
-            this.clientPanel.add(adultMalesLabel.get(this.numberOfHampersVisible));
-            this.clientPanel.add(adultMalesInput.get(this.numberOfHampersVisible));
-            this.clientPanel.add(adultFemalesLabel.get(this.numberOfHampersVisible));
-            this.clientPanel.add(adultFemalesInput.get(this.numberOfHampersVisible));
-            this.clientPanel.add(childrenOverEightLabel.get(this.numberOfHampersVisible));
-            this.clientPanel.add(childrenOverEightInput.get(this.numberOfHampersVisible));
-            this.clientPanel.add(childrenUnderEightLabel.get(this.numberOfHampersVisible));
-            this.clientPanel.add(childrenUnderEightInput.get(this.numberOfHampersVisible));
-            this.numberOfHampersVisible += 1; // add for next time to know
+            addNewHamperLabelsAndInputs();
 
             this.add(this.clientPanel, BorderLayout.CENTER);
 
@@ -98,19 +72,40 @@ public class Main extends JFrame implements ActionListener, MouseListener {
         }
     }
 
-    public void setupGUI() {
-
-        instructions = new JLabel("Please enter the number of clients.");
-
-        adultMalesLabel.add(new JLabel("Adult Males:"));
-        adultFemalesLabel.add(new JLabel("Adult Females:"));
-        childrenOverEightLabel.add(new JLabel("Children over 8:"));
-        childrenUnderEightLabel.add(new JLabel("Children under 8:"));
+    private void addNewHamperLabelsAndInputs() {
+        hamperNumberLabel.add(new JLabel("Hamper " + String.valueOf(numberOfHampersVisible + 1) + ":"));
+        Font font = new Font("Default", Font.BOLD, 16);
+        hamperNumberLabel.get(this.numberOfHampersVisible).setFont(font);
+        this.clientPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // padding to make it look not as packed
 
         adultMalesInput.add(new JTextField("0", 15));
         adultFemalesInput.add(new JTextField("0", 15));
         childrenOverEightInput.add(new JTextField("0", 15));
         childrenUnderEightInput.add(new JTextField("0", 15));
+        adultMalesLabel.add(new JLabel("Adult Males:"));
+        adultFemalesLabel.add(new JLabel("Adult Females:"));
+        childrenOverEightLabel.add(new JLabel("Children over 8:"));
+        childrenUnderEightLabel.add(new JLabel("Children under 8:"));
+
+        this.clientPanel.add(hamperNumberLabel.get(this.numberOfHampersVisible));
+        this.clientPanel.add(adultMalesLabel.get(this.numberOfHampersVisible));
+        this.clientPanel.add(adultMalesInput.get(this.numberOfHampersVisible));
+        this.clientPanel.add(adultFemalesLabel.get(this.numberOfHampersVisible));
+        this.clientPanel.add(adultFemalesInput.get(this.numberOfHampersVisible));
+        this.clientPanel.add(childrenOverEightLabel.get(this.numberOfHampersVisible));
+        this.clientPanel.add(childrenOverEightInput.get(this.numberOfHampersVisible));
+        this.clientPanel.add(childrenUnderEightLabel.get(this.numberOfHampersVisible));
+        this.clientPanel.add(childrenUnderEightInput.get(this.numberOfHampersVisible));
+        this.numberOfHampersVisible += 1; // add for next time to know
+    }
+
+    public void setupGUI() {
+
+        instructions = new JLabel("Please enter the number of clients.");
+
+        this.clientPanel = new JPanel();
+        this.clientPanel.setLayout(new BoxLayout(this.clientPanel, BoxLayout.Y_AXIS));
+        addNewHamperLabelsAndInputs(); // add labels and text fields to client panel
 
         adultMalesInput.get(0).addMouseListener(this);
         adultFemalesInput.get(0).addMouseListener(this);
@@ -127,9 +122,6 @@ public class Main extends JFrame implements ActionListener, MouseListener {
         JPanel headerPanel = new JPanel();
         headerPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
 
-        this.clientPanel = new JPanel();
-        this.clientPanel.setLayout(new BoxLayout(this.clientPanel, BoxLayout.Y_AXIS));
-
         this.submitPanel = new JPanel();
         this.submitPanel.setLayout(new FlowLayout(FlowLayout.TRAILING, 20, 25));
 
@@ -143,9 +135,7 @@ public class Main extends JFrame implements ActionListener, MouseListener {
         this.setMinimumSize(new Dimension(500, 400));
 
         headerPanel.add(instructions);
-        hamperNumberLabel.add(new JLabel("Hamper " + String.valueOf(numberOfHampersVisible) + ":"));
-        Font font = new Font("Default", Font.BOLD, 16);
-        hamperNumberLabel.get(0).setFont(font);
+
         this.clientPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         this.clientPanel.add(hamperNumberLabel.get(0));
         this.clientPanel.add(adultMalesLabel.get(0));
@@ -171,62 +161,45 @@ public class Main extends JFrame implements ActionListener, MouseListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            JOptionPane.showMessageDialog(null, "I was clicked and handled by buttonListener!");
-            reset();
+
+            if (!validateHamperInputs()) {
+                return;
+            }
+            callCreateHampers();
+            // dlg.setModal(false);
+            // dialog = JOptionPane.createDialog("Finding best combinations of food
+            // ...");//showMessageDialog(null, "Finding best combinations of food ...");
+            // reset();
 
         }
+
+    }
+
+    private void callCreateHampers() {
+
+        this.getContentPane().removeAll();
+        JPanel headerPanel = new JPanel();
+        headerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        headerPanel.add(new JLabel("Working... Please wait."));
+        this.add(headerPanel);
+        this.revalidate();
+        this.validate();
+        this.repaint();
+
+        // DO WORK HERE
+        JOptionPane.showMessageDialog(this, "Order Complete! Press OK to make additional orders.");
+        reset();
 
     }
 
     private void reset() {
-        this.dispose();
-        new Main().setVisible(true);
-        
-        // // no easy way to reset the gui so you literally have to remove components from
-        // // existing gui.
-        // for (int i = 1; i < this.numberOfHampersVisible; i++) {
-        //     this.setLayout(new BorderLayout());
-        //     this.clientPanel.remove(hamperNumberLabel.get(i));
-        //     this.clientPanel.remove(adultMalesLabel.get(i));
-        //     this.clientPanel.remove(adultMalesInput.get(i));
-        //     this.clientPanel.remove(adultFemalesLabel.get(i));
-        //     this.clientPanel.remove(adultFemalesInput.get(i));
-        //     this.clientPanel.remove(childrenOverEightLabel.get(i));
-        //     this.clientPanel.remove(childrenOverEightInput.get(i));
-        //     this.clientPanel.remove(childrenUnderEightLabel.get(i));
-        //     this.clientPanel.remove(childrenUnderEightInput.get(i));
-        // }
-        // this.numberOfHampersVisible = 1; // default
+        this.dispose(); // close current window
+        new Main().setVisible(true); // start up new window
 
-        // // this.add(panel, BorderLayout.CENTER); // add panel to window center
-        // // this.add(reset, BorderLayout.SOUTH); // add reset button to window bottom
-        // this.pack();
-
-        // this.validate();
-
-        // this.revalidate();
-        // this.setLocationByPlatform(true);
-        // this.setVisible(true);
-        // this.setMinimumSize(new Dimension(500, 500));
-        // // this.setSize(new Dimension(500, 500));
-        // this.repaint();
-        // removeAll();// or remove(JComponent)
-        // revalidate();
-        // repaint();
     }
 
     public void actionPerformed(ActionEvent event) {
-        int adultMales = Integer.parseInt(adultMalesInput.get(0).getText());
-        int adultFemales = Integer.parseInt(adultFemalesInput.get(0).getText());
-        int childrenOverEight = Integer.parseInt(childrenOverEightInput.get(0).getText());
-        int childrenUnderEight = Integer.parseInt(childrenUnderEightInput.get(0).getText());
-
-        if (validateInput()) {
-            String petID = idProcessing();
-            // JOptionPane.showMessageDialog(this, "Your pet's clinic ID is " + petID);
-        }
         addNewHamper();
-
     }
 
     public void mouseClicked(MouseEvent event) {
@@ -261,7 +234,7 @@ public class Main extends JFrame implements ActionListener, MouseListener {
 
     }
 
-    private String idProcessing() {
+    private String inputProcessing() {
 
         // String petID = new String(String.valueOf(adultMales.charAt(0)) +
         // String.valueOf(adultFemales.charAt(0))
@@ -272,37 +245,50 @@ public class Main extends JFrame implements ActionListener, MouseListener {
         return "NOT IMPLEMENTED";
     }
 
-    private boolean validateInput() {
 
-        boolean allInputValid = true;
+    private boolean validateHamperInputs() {
+        String text;
+        ArrayList<Integer> clientTypeFromUser = new ArrayList<Integer>();
 
-        // if (!Character.isUpperCase(firstName.charAt(0)) || firstName.length() < 2 ||
-        // firstName.length() > 26) {
-        // allInputValid = false;
-        // JOptionPane.showMessageDialog(this, firstName + " is an invalid name
-        // input.");
-        // }
+        // Create a 2D array of the hamper inputs
+        ArrayList<ArrayList<JTextField>> hamperInputs = new ArrayList<ArrayList<JTextField>>();
+        hamperInputs.add(adultMalesInput);
+        hamperInputs.add(adultFemalesInput);
+        hamperInputs.add(childrenOverEightInput);
+        hamperInputs.add(childrenUnderEightInput);
 
-        // if (!Character.isUpperCase(lastName.charAt(0)) || lastName.length() < 2 ||
-        // lastName.length() > 26) {
-        // allInputValid = false;
-        // JOptionPane.showMessageDialog(this, lastName + " is an invalid name input.");
-        // }
+        for (int clientType = 0; clientType < 4; clientType++) {
+            // not really sure what to call this, but it's an array across the hampers of the adultMale/adultFemale/children numbers.
+            ArrayList<JTextField> adultMalesForExample = hamperInputs.get(clientType); 
 
-        // if (!Character.isUpperCase(petName.charAt(0)) || petName.length() < 2 ||
-        // petName.length() > 26) {
-        // allInputValid = false;
-        // JOptionPane.showMessageDialog(this, petName + " is an invalid name input.");
-        // }
+            for (int hamperNumber = 0; hamperNumber < this.numberOfHampersVisible; hamperNumber++) {
+                text = adultMalesForExample.get(hamperNumber).getText();
 
-        // if (birthYear < 1922 || birthYear > 2022) {
-        // allInputValid = false;
-        // JOptionPane.showMessageDialog(this,
-        // birthYear + " is an invalid birth year. Pets must be born between 1922 and
-        // 2022.");
-        // }
+                if (!(text.length() >= 1)) {
+                    String errMsg = "Invalid input in Hamper " + String.valueOf(hamperNumber + 1) + ", field number "
+                            + String
+                                    .valueOf(clientType + 1)
+                            + ".";
+                    errMsg += "\nClient inputs must have a length of at least 1.";
+                    JOptionPane.showMessageDialog(this, errMsg);
+                    return false;
+                }
+                if (!text.matches("[0-9]+")) {
+                    String errMsg = "Invalid input hamper " + String.valueOf(hamperNumber + 1) + ", field number "
+                            + String
+                                    .valueOf(clientType + 1)
+                            + ".";
+                    ;
+                    errMsg += "\nClient inputs must be integers.";
+                    JOptionPane.showMessageDialog(this, errMsg);
+                    return false;
+                }
 
-        return allInputValid;
+            }
+
+        }
+
+        return true;
 
     }
 
